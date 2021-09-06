fb50062ef12dcc550ae6c982817b632ba4722332
deepak <doorvicecare@gmail.com>
2021-09-06 15:41:16 +0530

    friends and likes

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -3,6 +3,7 @@ const Post = require('../models/post');
 const commentsMailer = require('../mailers/comments_mailer');
 const queue = require('../config/kue');
 const commentEmailWorker = require('../workers/comment_email_worker');
+const Like = require('../models/like');
 
 
 module.exports.create = async function(req, res){
@@ -45,22 +46,45 @@ module.exports.create = async function(req, res){
   
 }
 
+
 module.exports.destroy = async function(req, res){
-    try {
+
+    try{
         let comment = await Comment.findById(req.params.id);
-        if(comment.user == req.user.id){
+
+        if (comment.user == req.user.id){
+
             let postId = comment.post;
+
             comment.remove();
-           let post =  Post.findByIdAndUpdate(postId, {$pull : {comments : req.params.id}});
-                return res.redirect('back');
-            
+
+            let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+            // CHANGE :: destroy the associated likes for this comment
+            await Like.deleteMany({likeable: comment._id, onModel: 'Comment'});
+
+
+            // send the comment id which was deleted back to the views
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
+
+            req.flash('success', 'Comment deleted!');
+
+            return res.redirect('back');
         }else{
-          return res.redirect('back');
+            req.flash('error', 'Unauthorized');
+            return res.redirect('back');
         }
-      
-    } catch (error) {
-        console.log('Error', error);
+    }catch(err){
+        req.flash('error', err);
         return;
     }
-  
+    
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,6 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
+const Like = require('../models/like');
 
 module.exports.create = async function(req, res){
     try {
@@ -9,6 +10,7 @@ module.exports.create = async function(req, res){
         });
 
         if(req.xhr){
+            Post = await post.populate('user', 'name').execPopulate();
             return res.status(200).json({
                 data:{
                     post:post
@@ -34,6 +36,12 @@ module.exports.destroy = async function(req, res){
         let post = await Post.findById(req.params.id);
         
         if(post.user == req.user.id){
+
+
+           await Like.deleteMany({likeable : post, onModel : 'Post'});
+           await Like.deleteMany({_id : {$in : post.comments}});
+
+
             post.remove();
 
            await Comment.deleteMany({post : req.params.id});
